7ef1af0ed5b32ba91ff87b105d65d33dec871f3b
Attila Németh <attilanemeth@gmx.de>
2020-06-19 16:11:30 +0200

    Start

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # OG:READER
+
+[![GitHub release](https://img.shields.io/github/release/attus74/og_reader.svg)](https://GitHub.com/attus74/og_reader/releases/)
--- src/Reader.php
+++ src/Reader.php
@@ -2,12 +2,36 @@
 
 namespace Attus\OgReader;
 
+use GuzzleHttp\Client;
+
 /**
  * OG:Reader
  *
- * @author Attila NÃ©meth
+ * @author Attila Németh
  * 16.06.2020
  */
 class Reader {
+  
+  // HTTP Client
+  private     $_client;
+  
+  // URL to Read From
+  private     $_url;
+  
+  // OG Values
+  private     $_ogValues;
+  
+  public function __construct(string $url) 
+  {
+    $this->_url = $url;
+    $this->_client = new Client();
+  }
+  
+  public function read(): void 
+  {
+    $response = $this->_client->read($this->_url);
+    var_dump($response);
+    die();
+  }
   
 }
