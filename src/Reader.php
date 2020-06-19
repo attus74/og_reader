1b24d813dc65045a597c5c7debebd48157bedbcd
Attila Németh <attilanemeth@gmx.de>
2020-06-19 16:47:06 +0200

    OG:Reader

--- src/Reader.php
+++ src/Reader.php
@@ -29,9 +29,45 @@ class Reader {
   
   public function read(): void 
   {
-    $response = $this->_client->read($this->_url);
-    var_dump($response);
-    die();
+    $response = $this->_client->get($this->_url);
+    if ($response->getStatusCode() == '200') {
+      $body = (string)$response->getBody();
+      libxml_use_internal_errors(true);
+      $dom = new \DOMDocument();
+      $dom->loadHTML($body);
+      $metas = $dom->getElementsByTagName('meta');
+      $matches = [];
+      $this->_ogValues = [];
+      for ($i = 0; $i < $metas->length; $i++) {
+        if (preg_match('/^og\:(.*?)$/', $metas->item($i)->getAttribute('property'), $matches)) {
+          $this->_ogValues[$matches[1]] = $metas->item($i)->getAttribute('content');
+        }
+      }
+    }
+  }
+  
+  /**
+   * All OG:Values of this web site
+   * @return array
+   */
+  public function getValues(): array
+  {
+    return $this->_ogValues;
+  }
+  
+  /**
+   * A Single OG:Value
+   * @param type $name
+   * @return string
+   */
+  public function getValue($name): string
+  {
+    if (array_key_exists($name, $this->_ogValues)) {
+      return $this->_ogValues[$name];
+    }
+    else {
+      return NULL;
+    }
   }
   
 }
